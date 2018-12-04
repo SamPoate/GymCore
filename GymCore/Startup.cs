6fe67138e4b6f4339c2bd137fe4c45eb84fbd468
Sam <sampoate@outlook.com>
2018-12-04 10:24:21 +0000

    Add project files.

---
+++ GymCore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymCore.Startup))]
+namespace GymCore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
